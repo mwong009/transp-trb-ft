5914dbfde584393f8f741c3b0d1d0d5a145c26cd
melvin <melvin.wong@ryerson.ca>
2018-10-28 23:58:36 -0400

    rename size to shape

--- transptrb/layers/generator.py
+++ transptrb/layers/generator.py
@@ -2,17 +2,17 @@ import theano
 import numpy as np
 
 
-def gen_param(name, size, rng=None):
+def gen_param(name, shape, rng=None):
     assert name in ['W', 'b', 'hbias', 'vbias']
-    assert len(size) > 0
+    assert len(shape) > 0
 
     if rng is None:
         rng = np.random.RandomState(888)
 
-    if len(size) == 1:
+    if len(shape) == 1:
         init_param = theano.shared(
             value=np.zeros(
-                shape=size,
+                shape=shape,
                 dtype=theano.config.floatX
             ),
             name=name,
@@ -23,9 +23,9 @@ def gen_param(name, size, rng=None):
         init_param = theano.shared(
             value=np.asarray(
                 rng.uniform(
-                    low=-np.sqrt(6. / np.sum(size)),
-                    high=np.sqrt(6. / np.sum(size)),
-                    size=size
+                    low=-np.sqrt(6. / np.sum(shape)),
+                    high=np.sqrt(6. / np.sum(shape)),
+                    size=shape
                 ),
                 dtype=theano.config.floatX
             ),
