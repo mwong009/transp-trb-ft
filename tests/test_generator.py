8964c16593df64e369a93d23b6de230bd7c3a616
melvin <melvin.wong@ryerson.ca>
2018-10-28 23:46:08 -0400

    create test_generator

---
+++ tests/test_generator.py
@@ -0,0 +1,16 @@
+import packageloader
+
+from unittest import TestCase
+from layers.generator import gen_param
+
+import numpy as np
+
+
+class GeneratorTest(TestCase):
+
+    def test_genParam(self):
+        rng = np.random.RandomState(888)
+        shape = (20, 2)
+        param = gen_param('W', shape, rng)
+
+        assert param.get_value().shape == shape
